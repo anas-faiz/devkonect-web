1a98d4e82bd3670197187e386f087b6597e037df
Anas-Faiz <anasfaiz0811@gmail.com>
2025-10-23 00:58:19 -0700

    sign up page made

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import SignUp from "./components/SignUP";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen w-full ">
       <Navbar/>
-      <Hero/>
+      <Routes>
+        <Route path='/' element={<Hero/>}/>
+        <Route path="/signUp" element={<SignUp/>} />
+      </Routes>
       <Footer/>
     </div>
   );
