22a568f28afa6717c10bceab2f71f12cdcb641e1
Anas-Faiz <anasfaiz0811@gmail.com>
2025-10-27 11:07:18 -0700

    feed page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,20 +1,35 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Card from "./Card";
+import axios from "axios";
+import { addFeed } from "../utils/feedSlice"
+import { useEffect } from "react";
 
-const Feed = ()=>{
-    const user = useSelector((store)=>store.user)
-
-    if(!user){
-        return(<div className="flex items-center justify-center h-screen">
+const Feed = () => {
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+    const getFeed= async () => {
+        const res = await axios.get(`${import.meta.env.VITE_API_URL}/feed`,
+            {withCredentials:true})
+            dispatch(addFeed(res.data.data))           
+        }
+        useEffect(()=>{
+            getFeed()
+        },[])
+  if (!feed) {
+    return (
+      <div className="flex items-center justify-center h-screen">
         <span className="loading loading-spinner text-amber-500"></span>
+        <h1 className="p-4 text-primary text-lg">
+          Getting <span className="text-secondary text-xl font-bold">Users</span>
+        </h1>
       </div>
-        )
-    }
-    return(
-        <div className="flex items-center justify-center">
-            <Card user={user}/>
-        </div>
-    )
-}
+    );
+  }
+  return (
+    <div className="flex items-center justify-center">
+      <Card feed={feed} />
+    </div>
+  );
+};
 
 export default Feed;
