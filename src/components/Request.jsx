0f76c72f8f665fe1caa8d30fbf39001e2c84c4d3
Anas-Faiz <anasfaiz0811@gmail.com>
2025-10-30 12:53:20 -0700

    request component

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -1,7 +1,20 @@
+import { useSelector } from "react-redux"
+import ConnectionCard from "./ConnectionCard";
+
 const Request = ()=>{
+    const request = useSelector((store)=> store.request);
+
+
+    if(!request) return (<div>Looking for request</div>)
+    
+    if(request.length == 0) return (<div>no requests yet</div>)
+
+        
     return(
         <div>
-            request
+            {request.map((request)=>(
+                <ConnectionCard data={request.fromUserId}/>
+            ))}
         </div>
     )
 }
