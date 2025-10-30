a6db6495bd42fd4f6bd3f44a3639f485ccb18743
Anas-Faiz <anasfaiz0811@gmail.com>
2025-10-30 12:38:53 -0700

    getRequest

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,37 +1,48 @@
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import { addFeed } from "../utils/feedSlice"
+import { addFeed } from "../utils/feedSlice";
 import { useEffect } from "react";
 import FeedCard from "./FeedCard";
+import { addRequest } from "../utils/requestSlice";
 
 const Feed = () => {
   const feed = useSelector((store) => store.feed);
   const dispatch = useDispatch();
-    const getFeed= async () => {
-        const res = await axios.get(`${import.meta.env.VITE_API_URL}/feed`,
-            {withCredentials:true})
-            dispatch(addFeed(res.data.data))           
-        }
-        useEffect(()=>{
-            getFeed()
-        },[])
+
+  const getRequest = async () => {
+    const res = await axios.get(
+      `${import.meta.env.VITE_API_URL}/user/requests/received`,
+      { withCredentials: true }
+    );
+    console.log(res.data.data);
+    dispatch(addRequest(res.data.data));
+  };
+  const getFeed = async () => {
+    const res = await axios.get(`${import.meta.env.VITE_API_URL}/feed`, {
+      withCredentials: true,
+    });
+    dispatch(addFeed(res.data.data));
+  };
+  useEffect(() => {
+    getRequest();
+    getFeed();
+  }, []);
   if (!feed) {
     return (
       <div className="flex items-center justify-center h-screen">
         <span className="loading loading-spinner text-amber-500"></span>
         <h1 className="p-4 text-primary text-lg">
-          Getting <span className="text-secondary text-xl font-bold">Users</span>
+          Getting{" "}
+          <span className="text-secondary text-xl font-bold">Users</span>
         </h1>
       </div>
     );
   }
   return (
     <div className="flex items-center justify-center">
-      {
-        feed.map((user)=>(
-          <FeedCard key={user._id} feed={user}/>
-        ))
-      }        
+      {feed.map((user) => (
+        <FeedCard key={user._id} feed={user} />
+      ))}
     </div>
   );
 };
