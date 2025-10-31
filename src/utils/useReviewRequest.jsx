fd6b5ac03ff16e93cff3c81c9f0b55f1119b1728
Anas-Faiz <anasfaiz0811@gmail.com>
2025-10-31 12:19:39 -0700

    remove request from ui logic

--- src/utils/useReviewRequest.jsx
+++ src/utils/useReviewRequest.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { removeRequest } from "./requestSlice";
 
 const useReviewRequest = async (status,_id)=>{
 
+    const dispatch = useDispatch();
+
     try {
         const res = await axios.post(`${import.meta.env.VITE_API_URL}/request/review/${status/_id}`,{},{withCredentials:true});
-        
+        dispatch(removeRequest())
 
     } catch (error) {
         console.log(error)
